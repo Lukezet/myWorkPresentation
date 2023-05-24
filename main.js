21ba2eea567dd0847adef501a411f548922fb767
Lukezet <lucasyanezbogni@hotmail.com>
2023-05-24 00:41:59 -0300

    ajustes responsive

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.menu-btn').addEventListener('click', ()=>{
+    document.querySelector('.nav-menu').classList.toggle('show')
+});     
+        //creamos el evento al dar click
+        //classlist para agregar una clase
+        //metodo toggle para que solo la cree si no existe. y si esta la quite 
